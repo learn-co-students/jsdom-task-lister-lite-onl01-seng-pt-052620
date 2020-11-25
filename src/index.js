9dde06fe5e6eb0f34d2449f1df93e4e81aef5026
bebofofum <bri.boehm@gmail.com>
2020-11-24 21:14:37 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,29 @@
-document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+// document.addEventListener("DOMContentLoaded", () => {
+
+//   // your code here
+// });
+
+// function getTaskItem() {
+//   const input = document.getElementById('input');
+//   input.addEventListener('click', function(event) {
+//     let taskItem = document.getElementById('new-task-description').value;
+//     return taskItem;
+//   });
+// }
+
+const taskForm = document.getElementById("create-task-form")
+
+const taskList = document.querySelector("#tasks")
+
+taskForm.addEventListener("submit", function(e) {
+  e.preventDefault()
+  const taskItem = document.querySelector("#new-task-description").value
+
+  const addItemList = document.createElement("li")
+  addItemList.innerText = taskItem
+
+  taskList.appendChild(addItemList)
+  taskForm.reset()
+
+})
+
