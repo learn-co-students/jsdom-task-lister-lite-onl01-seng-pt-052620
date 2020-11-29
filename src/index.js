4d612d5c06fec488fd2eec62425a7ed6fcf1820f
8bitbran <brandonmar97@gmail.com>
2020-11-29 17:42:08 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,24 +1,20 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
-});
+  const taskForm = document.querySelector("#create-task-form")
+  const taskList = document.querySelector("#tasks")
 
-
-const taskForm = document.querySelector("#create-task-form")
-const taskList = document.querySelector("#tasks")
-
-taskForm.addEventListener("submit", function(e) {
-  e.preventDefault()
-  const newTask = document.querySelector("#new-task-description").value
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+    const newTask = document.querySelector("#new-task-description").value
 
   // Add 'li' using innerHTML and +=
   // NOTE: this method effects the DOM immediately so the DOM will react to the innerHTML being reset
   // Also has security drawbacks
 
-  taskList.innerHTML += `
-      <li> ${newTask} 
-        <button data-action="delete">X</button>
-      </li>
-      `
+    taskList.innerHTML += `
+        <li> ${newTask} 
+          <button data-action="delete">X</button>
+        </li>
+        `
 
   // Add 'li' using createElement, innerText and appendChild
   // Preferred method
@@ -27,11 +23,14 @@ taskForm.addEventListener("submit", function(e) {
   // taskItem.innerText = newTask 
   // taskList.appendChild(taskItem)
 
-  taskForm.reset()
+    taskForm.reset()
 
-  taskList.addEventListener("click", function(e) {
-    if (e.target.dataset.action === "delete") {
-      e.target.parentElement.remove()
-    }
+    taskList.addEventListener("click", function(e) {
+      if (e.target.dataset.action === "delete") {
+        e.target.parentElement.remove()
+      }
+    })
   })
-})
+});
+
+
