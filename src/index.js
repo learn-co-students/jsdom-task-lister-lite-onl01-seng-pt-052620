18d494bcc861e78ae367900d94c6c0c2f0b600ff
lsuman83 <root@DESKTOP-3EM91F2.localdomain>
2020-11-30 18:29:28 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,11 @@
 document.addEventListener("DOMContentLoaded", () => {
   // your code here
-});
+  document.getElementById('create-task-form').addEventListener('click', function(event) {
+    let textBox =  document.getElementById('new-task-description').value;
+    let listText = document.getElementById('tasks')
+  
+    listText.innerText = textBox;
+    event.preventDefault();
+  }, false);});
+
+  
