831687dc023fbb9e2b46add6de6ff53807bf6e7e
Dmitriy Malayev <56614284+DmitriyMalayev@users.noreply.github.com>
2020-12-09 16:33:29 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,11 +1,21 @@
-// document.addEventListener("DOMContentLoaded", () => {
-//   event.preventDefault
+
+document.addEventListener("submit", preventSubmit)
 
 
-// });
-
-
-document.addEventListener("DOMContentLoaded", function(event){
-  event.preventDefault
+function preventSubmit(event){
+  event.preventDefault()   //Prevent default behavior 
+  const desc = document.getElementById("new-task-description")   //Make a constant and set it to the whole id input element  
+  const list_item = document.createElement("li")  //Create an li element 
+  list_item.innerHTML = desc.value   //Set the innerHTML of the list item equal to the value of desc 
+  document.getElementById("tasks").appendChild(list_item)  //select the id and append the list_item to the DOM
 }
-)
+
+
+
+
+
+// ## Deliverables:
+
+// - As a user, I should be able to type a task into the input field.
+// - As a user, I should be able to click some form of a submit button.
+// - As a user, the task string that I provided should appear on the DOM after the submit button has been activated.
