c44fea811ffe8e589325ad57f43e8bad3c108497
iamcid <iamcid17@gmail.com>
2020-12-09 23:45:50 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,23 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const main = document.querySelector("#main-content")
+  console.log(main)
+
+  const taskForm = document.getElementById("create-task-form")
+  const taskList = document.querySelector("#tasks")
+
+  taskForm.addEventListener("submit", function(e) {
+    e.preventDefault()
+  
+    const newTask = document.querySelector("#new-task-description").value
+
+    taskList.innerHTML += `<li> ${newTask} <button data-action="delete">delete</button> </li>`
+
+    taskForm.reset()
+  })
+
+  taskList.addEventListener("click", function(e) {
+    if (e.target.dataset.action === "delete") {
+      e.target.parentElement.remove()
+    }
+  })
 });
