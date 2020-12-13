8eaf3a96cc3248e1cfa09f4426c90dd2d5e5ce4e
angela stephens <angelastephens317@gmail.com>
2020-12-13 16:55:01 -0500

    Done.

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
+
 document.addEventListener("DOMContentLoaded", () => {
+  document.addEventListener("submit", (e) => {
+    e.preventDefault()
+   let id= document.getElementById("new-task-description").value
+   let id2= document.createElement("li")
+   id2.innerHTML = id
+   let list = document.getElementById("tasks")
+   list.appendChild(id2)
+  })
   // your code here
 });
